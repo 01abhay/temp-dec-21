63906b92e7e456bce7052036d2f14914b5457a17
AbhaY <pratapabhay540@gmail.com>
2024-12-21 21:53:19 +0530

    added amplitude

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as amplitude from '@amplitude/analytics-browser'
+
 import './index.css'
 import App from './App.tsx'
+
+amplitude.init('a3ae62b39acfd37c5db779339f11ba7b', { autocapture: true })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
